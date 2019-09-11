720bcdc4e5d13bc5d18ed09bc4b6d41980443cf2
Joseph Moon <nakkta@gmail.com>
2019-09-11 20:31:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,11 +24,6 @@ function dec(n){
   return n
 }
 
-function makeInt(n){
-  parseInt(n);
-  return n ;
-}
-
-function makeInt(n){
-  parseInt(n,10)
+function makeInt(n) {
+  return parseInt(n,10)
 }
